Fixed neighbor summation ovr wrong dimension, Code clean up

eagcn_pytorch/models.py
@@ -15,13 +15,12 @@ from sympy.ntheory import factorint
 
 
 class MolGraph(nn.Module):
-    def __init__(self, n_afeat, edge_to_ix, edge_word_len, node_to_ix, node_word_len, edge_embedding_dim=5,
-                 node_embedding_dim=5, radius=2, use_att=True):
+    def __init__(self, n_afeat, fp_len, edge_to_ix, edge_word_len, node_to_ix, node_word_len, radius, edge_embedding_dim, node_embedding_dim, use_att):
         super(MolGraph, self).__init__()
 
         self.radius = radius
-
         self.use_att = use_att
+        self.fp_len = fp_len
         self.edge_to_ix = edge_to_ix
         self.edge_word_len = edge_word_len
         self.edge_embed_len = edge_embedding_dim
@@ -30,65 +29,107 @@ class MolGraph(nn.Module):
         self.node_embed_len = node_embedding_dim
         self.node_attr_len = n_afeat - self.node_word_len + self.node_embed_len
         self.edge_embeddings = nn.Embedding(len(edge_to_ix), edge_embedding_dim)
-        self.edge_embed_bn = nn.BatchNorm1d(edge_embedding_dim)
         self.node_embeddings = nn.Embedding(len(node_to_ix), node_embedding_dim)
-        self.node_embed_bn = nn.BatchNorm1d(self.node_attr_len)
 
         self.conv = {}
         self.bn = {}
 
-        for rad in range(self.radius):
-            setattr(self, '_'.join(('conv', 'neighbor', str(rad))), nn.Conv2d(self.node_attr_len + self.edge_embed_len,
-                                                                              self.node_attr_len, kernel_size=1,
-                                                                              stride=1,
-                                                                              padding=0, dilation=1, groups=gcd(
-                    self.node_attr_len + self.edge_embed_len, self.node_attr_len), bias=False))
-            self.conv[('neighbor', rad)] = getattr(self, '_'.join(('conv', 'neighbor', str(rad))))
-            setattr(self, '_'.join(('bn', 'neighbor', str(rad))), nn.BatchNorm1d(self.node_attr_len))
-            self.bn[('neighbor', rad)] = getattr(self, '_'.join(('bn', 'neighbor', str(rad))))
+        for rad in range(radius):
+            setattr(self, '_'.join(('conv', 'node', str(rad))),
+                    to_hw(nn.Conv1d(self.node_attr_len, self.node_attr_len, kernel_size=1,
+                                    stride=1, padding=0, dilation=1, groups=Shi_GCN.largest_divisor(self.node_attr_len),
+                                    bias=False)))
+            self.conv[('node', rad)] = getattr(self, '_'.join(('conv', 'node', str(rad))))
 
+            setattr(self, '_'.join(('conv', 'out', str(rad))), to_hw(nn.Conv1d(self.node_attr_len, fp_len, kernel_size=1,
+                                                                               stride=1, padding=0, dilation=1,
+                                                                               groups=gcd(self.node_attr_len, fp_len),
+                                                                               bias=False)))
+            self.conv[('out', rad)] = getattr(self, '_'.join(('conv', 'out', str(rad))))
+
+            setattr(self, '_'.join(('bn', 'out', str(rad))), to_hw(nn.BatchNorm1d(fp_len, affine=False)))
+            self.bn[('out', rad)] = getattr(self, '_'.join(('bn', 'out', str(rad))))
+
+            setattr(self, '_'.join(('conv', 'neighbor', str(rad))),
+                    to_hw(nn.Conv2d(self.node_attr_len + self.edge_embed_len,
+                                    self.node_attr_len, kernel_size=1,
+                                    stride=1,
+                                    padding=0, dilation=1, groups=gcd(
+                            self.node_attr_len + self.edge_embed_len, self.node_attr_len), bias=False)))
+            self.conv[('neighbor', rad)] = getattr(self, '_'.join(('conv', 'neighbor', str(rad))))
+
+            setattr(self, '_'.join(('conv', 'edge', str(rad))),
+                    to_hw(nn.Conv2d(self.edge_embed_len, self.edge_embed_len,
+                                    kernel_size=1, stride=1, padding=0,
+                                    dilation=1,
+                                    groups=Shi_GCN.largest_divisor(
+                                        self.edge_embed_len), bias=False)))
+            self.conv[('edge', rad)] = getattr(self, '_'.join(('conv', 'edge', str(rad))))
 
     def embed_edges(self, adjs, bfts):
         nz = adjs.view(-1).byte()
         new_edges = Variable(from_numpy(np.zeros(nz.shape + (self.edge_embed_len,))).float())
-        new_edges[nz.unsqueeze(1).expand(-1, self.edge_embed_len)] = self.edge_embed_bn(self.edge_embeddings(bfts)).view(-1)
-        new_edges = new_edges.view(adjs.shape + (-1,)).permute(0, 3, 1, 2).contiguous()
-        return torch.mul(new_edges, adjs.unsqueeze(1).expand(-1, self.edge_embed_len, -1, -1))
+        new_edges[nz.unsqueeze(1).expand(-1, self.edge_embed_len)] = self.edge_embeddings(bfts).view(-1)
+        return new_edges.view(adjs.shape + (-1,)).permute(0, 3, 1, 2).contiguous()
+        # return torch.mul(new_edges, adjs.unsqueeze(1).expand(-1, self.edge_embed_len, -1, -1))
 
     def embed_nodes(self, adjs, afms, axfms):
         nz, _ = adjs.max(dim=2)
         nz = nz.view(-1).byte()
         new_nodes = Variable(from_numpy(np.zeros(nz.shape + (self.node_attr_len,))).float())
-        nodes = torch.cat((self.node_embeddings(afms), axfms), dim=1)
-        new_nodes[nz.unsqueeze(1).expand(-1, self.node_attr_len)] = self.node_embed_bn(nodes).view(-1)
+        new_nodes[nz.unsqueeze(1).expand(-1, self.node_attr_len)] = torch.cat((self.node_embeddings(afms), axfms), dim=1).view(-1) # self.node_embeddings(afms).view(-1)
 
-        new_nodes = new_nodes.view(adjs.shape[0:-1] + (-1,)).permute(0, 2, 1).contiguous()
+        # new_nodes = new_nodes.view(adjs.shape[0:-1] + (-1,)).permute(0, 2, 1).contiguous()
+        return new_nodes.view(adjs.shape[0:-1] + (-1,)).permute(0, 2, 1).contiguous()
+        # return torch.mul(new_nodes, nz.view(adjs.shape[0:-1]).unsqueeze(1).expand(-1, self.node_attr_len, -1).float())
 
-        return torch.mul(new_nodes, nz.view(adjs.shape[0:-1]).unsqueeze(1).expand(-1, self.node_attr_len, -1).float())
+    def get_node_activation(self, nz, node_data, layer):
+        out = self.conv[('out', layer)](node_data)
+        out = torch.mul(out, nz.unsqueeze(1).expand(-1, self.fp_len, -1))
+        out = torch.sum(out, dim=2)
+        # F.elu seems to work better here? is it because I don't normalize the the node and edges?
+        # Should I drop the dropout? It seems to cause issues
+        # F.dropout is bad?
+        # return F.dropout(F.relu(self.bn[('out', layer)](out)), p=self.dropout, training=self.training)
+        return out
+
+    def get_next_node(self, nz, node_data, layer):
+        out = self.conv[('node', layer)](node_data)
+        # out = torch.mul(out, nz.unsqueeze(1).expand(-1, self.node_attr_len, -1))
+        return out
+
+    def get_next_edge(self, adj, edge_data, layer):
+        out = self.conv[('edge', layer)](edge_data)
+        # out = torch.mul(adj.unsqueeze(1).expand(-1, self.edge_embed_len, -1, -1), out)
+        return out
 
     def get_neighbor_act(self, adj_mat, node_data, edge_data, layer):
         lna = torch.mul(adj_mat.unsqueeze(1).expand(-1, self.node_attr_len, -1, -1), node_data.unsqueeze(3))
         lnb = torch.mul(adj_mat.unsqueeze(1).expand((-1, self.edge_embed_len, -1, -1)), edge_data)
         ln = torch.cat((lna, lnb), dim=1)
         ln = self.conv[('neighbor', layer)](ln)
-        ln = torch.mul(adj_mat.unsqueeze(1).expand((-1, self.node_attr_len, -1, -1)), ln).permute(0, 2, 3, 1)
-        ln[adj_mat.unsqueeze(3).expand(-1, -1, -1, self.node_attr_len).byte()] = self.bn[('neighbor', layer)](
-                ln[adj_mat.unsqueeze(3).expand(-1, -1, -1, self.node_attr_len).byte()].view(-1, self.node_attr_len))
-        ln = F.relu(ln.permute(0, 3, 1, 2).contiguous())
-        ln = ln.sum(dim=3)
-        nz, _ = adj_mat.max(dim=2)
-        nz = nz.view(-1).byte()
-        ln = torch.mul(nz.view(adj_mat.shape[0:-1]).unsqueeze(1).expand(-1, self.node_attr_len, -1).float(), ln)
+        # ln = torch.mul(adj_mat.unsqueeze(1).expand((-1, self.node_attr_len, -1, -1)), ln)
+        # TODO: We Should let the machine decide how it would like to summarize the neighbor data
+        ln = ln.sum(dim=2)
+        # nz, _ = adj_mat.max(dim=2)
+        # nz = nz.view(-1)
+        # ln = torch.mul(nz.view(adj_mat.shape[0:-1]).unsqueeze(1).expand(-1, self.node_attr_len, -1), ln)
         return ln
 
     def next_radius_adj_mat(self, adj_mat, adj_mats):
         next_adj_mat = torch.bmm(adj_mat, adj_mats[0])
-        next_adj_mat[next_adj_mat != 0] = 1
+        next_adj_mat[next_adj_mat!=0] = 1
         next_adj_mat = next_adj_mat - sum(adj_mats)
         next_adj_mat = torch.clamp(next_adj_mat - Variable(from_numpy(np.eye(next_adj_mat.size()[1])*next_adj_mat.size()[1]).float()), min=0)
         return next_adj_mat
 
     def forward(self, adjs, afms, axfms, bfts):
+        if self.use_att:
+            return self.att_forward(adjs, afms, axfms, bfts)
+        else:
+            return self.act_forward(adjs, afms, axfms, bfts)
+
+    def act_forward(self, adjs, afms, axfms, bfts):
         edge_data = self.embed_edges(adjs, bfts)
         node_data = self.embed_nodes(adjs, afms, axfms)
 
@@ -96,32 +137,70 @@ class MolGraph(nn.Module):
 
         nz, _ = adjs.max(dim=2)
 
-        layers = []
+        fps = []
 
         node_current = node_data
         edge_current = edge_data
         adj_mat = adjs_no_diag
         adj_mats = []
 
+        # TODO: Should we conv edge_data (edge_current) for next iteration?
+        # TODO: Should we conv node_data (node_current) for next iteration?
+
         for radius in range(self.radius):
-            layers.append(node_current)
+            fp = self.get_node_activation(nz, node_current, radius)
+            fps.append(fp)
 
-            node_next = node_current
-
-            # get neighbor activation
-            neighbor_next = self.get_neighbor_act(adj_mat, node_current, edge_current, radius)
-            if self.use_att:
-                node_next = torch.mul(node_next, neighbor_next)
-            else:
-                node_next = node_next + neighbor_next
+            node_next = self.get_next_node(nz, node_current, radius)
+            neighbor_next = self.get_neighbor_act(adj_mat, node_current, edge_current,
+                                                  radius)  # get neighbor activation
+            node_next = node_next + neighbor_next
 
             edge_next = torch.matmul(adj_mat.unsqueeze(1).expand((-1, self.edge_embed_len, -1, -1)), edge_data)
             adj_mats.append(adj_mat)
             adj_mat = self.next_radius_adj_mat(adj_mat, adj_mats)
+            edge_next = self.get_next_edge(adj_mat, edge_next, radius)
 
             node_current, edge_current = node_next, edge_next
 
-        return layers
+        return fps
+
+    def att_forward(self, adjs, afms, axfms, bfts):  # bfts
+        edge_data = self.embed_edges(adjs, bfts)
+        node_data = self.embed_nodes(adjs, afms, axfms)
+
+        adjs_no_diag = torch.clamp(adjs - Variable(from_numpy(np.eye(adjs.size()[1])).float()), min=0)
+
+        nz, _ = adjs.max(dim=2)
+
+        fps = []
+
+        node_current = node_data
+        edge_current = edge_data
+        adj_mat = adjs_no_diag
+        adj_mats = []
+
+        # TODO: Should we conv edge_data (edge_current) for next iteration? - Yes for now
+        # TODO: Should we conv node_data (node_current) for next iteration? - Yes for now
+
+        for radius in range(self.radius):
+            fp = self.get_node_activation(nz, node_current, radius)
+            fps.append(fp)
+
+            node_next = self.get_next_node(nz, node_current, radius) # node_current
+            neighbor_next = self.get_neighbor_act(adj_mat, node_current, edge_current, radius) # get neighbor activation
+            # TODO: Should let the machine decide how to take the neighbor data into account
+            # by summing? by multiplying?
+            node_next = torch.mul(node_next, neighbor_next)
+
+            edge_next = torch.matmul(adj_mat.unsqueeze(1).expand((-1, self.edge_embed_len, -1, -1)), edge_current)
+            adj_mats.append(adj_mat)
+            adj_mat = self.next_radius_adj_mat(adj_mat, adj_mats)
+            edge_next = self.get_next_edge(adj_mat, edge_next, radius)
+
+            node_current, edge_current = node_next, edge_next
+
+        return fps
 
 class ConcatModule(nn.Module):
     def __init__(self):
@@ -133,12 +212,12 @@ class ConcatModule(nn.Module):
 
 class MolGCN(nn.Module):
     def __init__(self, n_afeat, edge_to_ix, edge_word_len, node_to_ix, node_word_len, nclass, edge_embedding_dim=5,
-                 node_embedding_dim=5, radius=3, use_att=True):
+                 node_embedding_dim=5, radius=3, use_att=True, fp_len=50):
         super(MolGCN, self).__init__()
         self.radius = radius + 1
 
-        # self.molgraph = MolGraph(n_afeat, edge_to_ix, edge_word_len, node_to_ix, node_word_len,edge_embedding_dim, node_embedding_dim, self.radius, use_att)
-        self.molgraph = Shi_GCN(0, n_afeat, 10, 10, 10, 10, 10, 0, 0, 0, 0, 1, 1, 1, 1, 0.3, edge_to_ix, edge_word_len, node_to_ix, node_word_len)
+        self.molgraph = MolGraph(n_afeat, fp_len, edge_to_ix, edge_word_len, node_to_ix, node_word_len, self.radius, edge_embedding_dim, node_embedding_dim, use_att)
+        # self.molgraph = Shi_GCN(0, n_afeat, 10, 10, 10, 10, 10, 0, 0, 0, 0, 1, 1, 1, 1, 0.3, edge_to_ix, edge_word_len, node_to_ix, node_word_len)
         self.concat = ConcatModule()
 
         # no_stages = np.ceil(np.log2(n_afeat - node_word_len + node_embedding_dim))
@@ -151,9 +230,9 @@ class MolGCN(nn.Module):
 
         self.stages = nn.Conv2d(self.radius, 1, 1, bias=False)
 
-        self.bn = nn.BatchNorm1d(50)
+        self.bn = nn.BatchNorm1d(fp_len, affine=False)
 
-        self.classifier = nn.Linear(50, nclass)
+        self.classifier = nn.Linear(fp_len, nclass)
 
     def _make_layer(self, in_channels, out_channels):
         return nn.Sequential(