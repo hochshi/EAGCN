Fixed variance calc error in node data

eagcn_pytorch/SkipGramModel.py
@@ -163,9 +163,10 @@ class SkipGramMolEmbed(nn.Module):
 
 
     def batch_norm_nodes(self, node_data, adjs_diag_only, eps=1e-6):
-        mean = node_data.sum().div(adjs_diag_only.float().sum())
-        var = node_data.pow(2).sum().div(adjs_diag_only.float().sum()) - mean.pow(2) + eps
-        return (node_data-mean).div(var.sqrt()).mul(adjs_diag_only.sum(dim=-2).unsqueeze(2).float())
+        nz = adjs_diag_only.max(dim=2)[0].float().unsqueeze(2).expand(-1, -1, self.fp_len)
+        mean = node_data.sum().div(nz.float().sum())
+        var = node_data.pow(2).sum().div(nz.float().sum()) - mean.pow(2) + eps
+        return (node_data-mean).div(var.sqrt()).mul(nz)
 
 
     def batch_norm_edges(self, edge_data, adjs_no_diag, eps=1e-6):

eagcn_pytorch/train.py
@@ -347,7 +347,7 @@ def test_sgn_model(model, train_loader, test_loader):
         for j, topk in enumerate(top_ks):
             nearestn = dist_mats <= top_sim[:, topk-1].unsqueeze(1)
             nn_labels = torch.matmul(nearestn, train_labels)
-            correct[j] += (torch.mul(nn_labels, test_labels) > 0).sum().data[0]
+            correct[j] += (torch.mul(nn_labels, test_labels) > 0).sum().item()
     return np.true_divide(correct, total).tolist()
 
 
@@ -426,7 +426,7 @@ def train(tasks, EAGCN_structure, n_den1, n_den2, file_name):
 
     loss_hist = []
     process_bar0 = trange(num_epochs)
-    test_wrapper(model, train_loader, validation_loader)
+    # test_wrapper(model, train_loader, validation_loader)
     for epoch in process_bar0:
         tot_loss = 0
         model.train()
@@ -441,8 +441,8 @@ def train(tasks, EAGCN_structure, n_den1, n_den2, file_name):
         tqdm.write('Epoch: {}, Loss: {}'.format(epoch, tot_loss))
 
         # report performance
-        if epoch > 0 and 0 == epoch % 10:
-            test_wrapper(model, train_loader, validation_loader)
+        # if epoch > 0 and 0 == epoch % 10:
+        #     test_wrapper(model, train_loader, validation_loader)
     tqdm.write('Loss history: {}'.format(', '.join(map(str, loss_hist))))
     test_wrapper(model, train_loader, validation_loader)
     test_wrapper(model, train_loader, test_loader)