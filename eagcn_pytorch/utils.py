d6e64cb3909226b5191a7e784cb7247af5f4ba91
hochshi <hochshi@gmail.com>
2018-06-14 17:00:16 +0300

    Fixing free_solv graph error

--- eagcn_pytorch/utils.py
+++ eagcn_pytorch/utils.py
@@ -166,8 +166,7 @@ def load_freesolv(path='../data/', dataset = 'SAMPL.csv', bondtype_freq = 3,
         except AttributeError:
             print('the {}th row has an error'.format(i))
             error_row.append(i)
-        except ValueError:
-        except TypeError:
+        except (TypeError, ValueError):
             print('the {}th row smile is: {}, can not convert to graph structure'.format(i, data[i][1]))
             error_row.append(i)
         i += 1
